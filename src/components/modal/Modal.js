6c842b79f93fc5d626b9685d474b8f20f97e621c
mahmood114 <mahmoodalmohri@gmail.com>
2021-07-04 18:04:16 +0300

    Added priority option

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -32,6 +32,20 @@ const AddModal = (props) => {
                     <label className="form-label">Description</label>
                     <textarea type="text" className="form-control" name="description" onChange={handleChange} />
                 </div>
+                <div class="row mb-0">
+                    <div class="row justify-content-start">
+                        <div class="col-12">
+                            <div className="space-between-the-radio">
+                                <input type="radio" name="priority" id="high" value="3" onChange={handleChange} />
+                                <label for="high">High</label>
+                                <input type="radio" name="priority" id="moderate" value="2" onChange={handleChange} />
+                                <label for="moderate">Moderate</label>
+                                <input type="radio" name="priority" id="low" value="1" onChange={handleChange} />
+                                <label for="low">Low</label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <button type="submit" className="todo-button">Add Task</button>
             </form>
         </ModalStyle>
